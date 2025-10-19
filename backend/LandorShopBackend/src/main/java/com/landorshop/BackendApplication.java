8146adb11f7bb4d64b694cea34ed77532ba710fb
Wrieni <divanniyexpert76@gmail.com>
2025-10-19 23:52:58 +0300

    delete project

---
+++ backend/LandorShopBackend/src/main/java/com/landorshop/BackendApplication.java
@@ -0,0 +1,10 @@
+package com.landorshop;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication.java {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+}
---
+++ backend/LandorShopBackend/src/main/java/com/landorshop/Infrastructure/Services/ProductsService.java
@@ -0,0 +1,2 @@
+package com.landorshop.Infrastructure.Services;public class ProductsService {
+}
