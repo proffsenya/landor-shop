dc96c019a72ebedbee4146ae3baf382cd473d086
Wrieni <divanniyexpert76@gmail.com>
2025-10-20 16:26:26 +0300

    создание папки Infrastructure и UserRepository с минимальными методами findById, findByEmail, findAll, byIsActive

---
+++ backend/src/main/java/com/example/backend/Infrastructure/Repos/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.backend.Infrastructure.Repos;
+
+import com.example.backend.Domain.Models.User;
+import org.springframework.data.repository.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends Repository<User, Long> {
+    Optional<User> findById(Long id);
+    Optional<User> findByEmail(String email);
+    List<User> findAll();
+    List<User> byIsActive(Boolean isActive);
+}
