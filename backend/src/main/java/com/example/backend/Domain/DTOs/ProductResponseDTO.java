332715d8c0e37d48e3dfea817a72ca333c87ea63
proffsenya <arsenii.nsk@mail.ru>
2025-12-18 19:43:53 +0300

    фикс длины поля

--- backend/src/main/java/com/example/backend/Domain/DTOs/ProductResponseDTO.java
+++ backend/src/main/java/com/example/backend/Domain/DTOs/ProductResponseDTO.java
@@ -11,7 +11,7 @@ import java.util.List;
  */
 public record ProductResponseDTO(Long id, @Size(max = 100) String name, @Size(max = 2000) String description,
                                  @Size(max = 500) String guaranteedIndicators,
-                                 @Size(max = 100) String feedingNote,
+                                 @Size(max = 1000) String feedingNote,
                                  @Size(max = 100) String slug,
                                  Boolean isActive, Boolean isFeatured, BigDecimal rating,
                                  List<BreedDTO> breedsDTOs,
