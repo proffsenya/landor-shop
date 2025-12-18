988ee297cac138f47ded23848841b1688d659241
proffsenya <arsenii.nsk@mail.ru>
2025-12-18 19:16:36 +0300

    фикс

--- backend/src/main/java/com/example/backend/Domain/DTOs/UpdateProductDTO.java
+++ backend/src/main/java/com/example/backend/Domain/DTOs/UpdateProductDTO.java
@@ -10,7 +10,7 @@ import java.util.List;
 /**
  * DTO for {@link com.example.backend.Domain.Models.Product}
  */
-public record UpdateProductDTO(@Size(max = 100) String name, @Size(max = 1000) String description,
+public record UpdateProductDTO(@Size(max = 100) String name, @Size(max = 2000) String description,
                                @Size(max = 500) String guaranteedIndicators,
                                @Size(max = 100) String feedingNote,
                                @Size(max = 100) String slug,
